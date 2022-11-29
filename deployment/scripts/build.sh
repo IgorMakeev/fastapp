9cc838c5f860ec488647f40939d1e3e80dec37bb
Igor Makeev <i.makeev@a1qa.com>
2022-11-29 23:48:08 +0100

    Adding deployment scripts

--- deployment/scripts/build.sh
+++ deployment/scripts/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# logging in to aws ecr
-aws ecr get-login-password \
-   --region ${REGION} \
-   | docker login \
-   --username AWS \
-   --password-stdin ${REPOSITORY}
-
 # build
 docker build -t jenkins-builds .
--- deployment/scripts/push.sh
+++ deployment/scripts/push.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# logging in to aws ecr
+aws ecr get-login-password \
+   --region ${REGION} \
+   | docker login \
+   --username AWS \
+   --password-stdin ${REPOSITORY}
+
 # push
 docker tag jenkins-builds:latest ${REPOSITORY}/${APP_NAME}-${ENV}:latest
 docker push ${REPOSITORY}/${APP_NAME}-${ENV}:latest
