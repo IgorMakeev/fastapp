5e3dcaf67d4af44d45af3d4f7fccb51c87dd4435
Igor Makeev <i.makeev@a1qa.com>
2022-11-30 00:09:02 +0100

    Adding deployment scripts

--- deployment/scripts/push.sh
+++ deployment/scripts/push.sh
@@ -9,5 +9,5 @@ aws ecr get-login-password \
 
 # push
 docker tag jenkins-builds:latest ${REPOSITORY}/${APP_NAME}-${ENV}:latest
-docker push ${REPOSITORY}/${APP_NAME}-${ENV}:latest
+docker push ${REPOSITORY}/jenkins-builds/${APP_NAME}-${ENV}:latest
 # docker rmi -f $(docker images -q)
