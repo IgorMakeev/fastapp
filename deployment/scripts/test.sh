ab3ac3e60564bd741fd3f517e4e2c6cb845c3b10
Igor Makeev <i.makeev@a1qa.com>
2022-12-01 21:59:45 +0100

    Adding tests

--- deployment/scripts/test.sh
+++ deployment/scripts/test.sh
@@ -2,6 +2,12 @@
 
 docker compose -f ./deployment/docker-compose.yaml -d
 
+cat << EOF > ./deployment/.env
+POSTGRES_USER=${POSTGRES_USER}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+POSTGRES_DB=${POSTGRES_DB}
+EOF
+
 docker exec app pytest
 
 docker compose down -f ./deployment/docker-compose.yaml
