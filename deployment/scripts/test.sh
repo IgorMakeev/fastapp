523333735d23f7c532498ce77847737999ff5d39
Igor Makeev <i.makeev@a1qa.com>
2022-12-02 23:39:44 +0100

    Changing app, testing deploy

--- deployment/scripts/test.sh
+++ deployment/scripts/test.sh
@@ -10,6 +10,9 @@ docker compose -f ./deployment/docker-compose.yaml up  -d
 
 docker exec app pytest
 
+failure=`docker exec app pytest | grep 'FAILED' | awk '{print $1}'`
+if [ -z "$failure" ]; then echo "passed"; else exit 1; fi
+
 docker rm -f $(docker ps -aq)
 
 rm ./deployment/.env
