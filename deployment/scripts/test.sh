bffe020aa81a52724839d36dfaa78841c1a6b53f
Igor Makeev <i.makeev@a1qa.com>
2022-12-02 21:43:12 +0100

    Adding tests

--- deployment/scripts/test.sh
+++ deployment/scripts/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker compose -f ./deployment/docker-compose.yaml up -d
-
 cat << EOF > ./deployment/.env
 POSTGRES_USER=app
 POSTGRES_PASSWORD=app
 POSTGRES_DB=app
 EOF
+
+docker compose -f ./deployment/docker-compose.yaml up -d
 
 docker exec app pytest
 
